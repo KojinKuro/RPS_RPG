8e9c5748666dad3a1965d94350f0c4cf087aed71
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-28 12:44:20 -0800

    feature: Print text to console after button click
    After you click a button, it's innerText will be printed to console.
    This was done to debug and test linking things together while using
    webpack to make sure thing's still work.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 import "normalize.css";
+import {} from "./basic.js";
 import "./style.scss";
+
+console.log("test");
+
+function isButton(node) {
+  return node.tagName.toLowerCase() === "button";
+}
+
+var mainView = document.querySelector(".main-view");
+mainView.addEventListener("click", function (event) {
+  if (isButton(event.target)) console.log(event.target.innerText);
+});
