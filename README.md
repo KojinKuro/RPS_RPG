0809f6cb2a0222f3203a9d5dd58bb2ab3b6c5add
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-28 11:33:56 -0800

    Setup webpack

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Description
 
 This is a game that turns the classic game of Rock Paper into a JRPG-style game.
+
+Running the project source code requires use of Node LTS v20.11.1 due to some NPM packages installed.
---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
