5ba991d06617990058f4e3ef1638666e3a723b60
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-28 12:45:29 -0800

    feature: Hoist a function the global window
    A function was hoisted to the global window object so it can be called
    from the browser console. This will be removed later, but it is good
    knowing that this can be done. Previously functions couldn't be run
    from the console due to not knowing how to do this.

--- src/main.js
+++ src/main.js
@@ -12,3 +12,26 @@ var mainView = document.querySelector(".main-view");
 mainView.addEventListener("click", function (event) {
   if (isButton(event.target)) console.log(event.target.innerText);
 });
+
+window.createGame = function () {
+  let moves = ["Rock", "Paper", "Scissors"];
+
+  class Entity {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+
+  function inputPlayerMove(move) {
+    checkWinner(move);
+  }
+
+  function inputComputerMove() {}
+
+  function randomNumber(number) {
+    let randomNumber = Math.floor(Math.random() * (number + 1));
+    return randomNumber;
+  }
+
+  return { randomNumber };
+};
