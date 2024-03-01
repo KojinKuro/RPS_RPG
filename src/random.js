ead3e955827ef0ea14afe3e212949777aee276b9
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-29 19:35:54 -0800

    Add images

--- src/random.js
+++
@@ -1,5 +0,0 @@
-// return a number from 0 to number including 0 and number
-export function randomNumber(number) {
-  let randomNumber = Math.floor(Math.random() * (number + 1));
-  return randomNumber;
-}
