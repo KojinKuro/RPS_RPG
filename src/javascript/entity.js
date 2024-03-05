b9373ede95fc56511edc0a472e9cf44299c82034
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-04 22:16:13 -0800

    Track points and rounds now

--- src/javascript/entity.js
+++ src/javascript/entity.js
@@ -1,54 +1,33 @@
-import { AlertManager } from "./alert.js";
 import { randomNumber } from "./random.js";
+import { Round } from "./round.js";
 
 export class Entity {
-  constructor(name, movesList, level, healthManager) {
+  constructor(name, movesList, healthManager) {
     this.name = name;
     this.movesList = movesList;
     this.points = 0;
 
-    this.level = level;
-    this.maxHealth = this.level;
-    this.currentHealth = this.maxHealth;
     this.healthManager = healthManager;
     this.updateHealth();
-  }
-
-  setLevel(level) {
-    this.level = level;
-    this.maxHealth = this.level;
   }
 
   addPoint(pointAdd = 1) {
     this.points += pointAdd;
   }
 
-  damage(damage = 1) {
-    this.currentHealth -= damage;
-    if (this.currentHealth < 0) this.currentHealth = 0;
-    this.updateHealth();
-  }
-
   updateHealth() {
-    this.healthManager.setHealth(this.currentHealth, this.maxHealth);
-  }
-
-  heal(addHealth = 1) {
-    if (this.currentHealth + addHealth > this.maxHealth)
-      this.currentHealth = this.maxHealth;
-    else this.currentHealth += addHealth;
-    this.updateHealth();
+    this.healthManager.setHealth(this.points, Round.getRound());
   }
 
   onWin() {
-    AlertManager.sendAlert(`${this.name} won!`);
+    global.dialogBox.displayContent(`${this.name} won!`);
+    Round.increaseRound();
     this.addPoint();
   }
 
   setMove(moveIndex) {
     if (!arguments.length) moveIndex = randomNumber(this.movesList.length - 1);
     moveIndex = Math.abs(moveIndex) % this.movesList.length;
-
     this.move = this.movesList[moveIndex];
   }
 
@@ -65,11 +44,6 @@ export class Entity {
       }
     });
 
-    let damageAmount = 1;
-    loser.damage(damageAmount);
-    AlertManager.sendAlert(
-      `${winner.name} deals ${damageAmount} damage to ${loser.name}`
-    );
-    if (!loser.currentHealth) return winner;
+    return winner;
   }
 }
